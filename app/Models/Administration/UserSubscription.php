c53d195008edd53c2e31bc1621a26cf460262a38
felcast <felipe.castillo@epayco.com>
2025-04-09 17:01:39 -0400

    Feat:detail module for users

--- app/Models/Administration/UserSubscription.php
+++ app/Models/Administration/UserSubscription.php
@@ -1,18 +1,14 @@
 <?php
+
 namespace App\Models\Administration;
 
-use App\Models\Status;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Model;
 
 class UserSubscription extends Model
 {
     use HasFactory;
 
-    protected $table = "administration.user_subscriptions";
-
-    public function status()
-    {
-        return $this->belongsTo(Status::class, 'status_id');
-    }
+    protected $table="administration.user_subscriptions";
+    
 }
