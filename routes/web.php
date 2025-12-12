cab30b1de4e594149ca53bc7bd271144decdd666
felcast <felipe.castillo@epayco.com>
2025-12-12 14:01:01 -0400

    TASK-003 Fix: Adjustments in paypal feature

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Carbon\Carbon;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,18 +15,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/failed-payment-preview', function () {
-    $transactionId     = 'TXN123456789';
-    $planName          = 'Plan Premium Mensual';
-    $lastFailedPayment = Carbon::now()->format('d/m/Y');
-
-    $data = [
-        'shipping_amount' => [
-            'value' => '9.99',
-        ],
-    ];
-
-    return view('emails.subscriptions.failedPayment', compact('transactionId', 'planName', 'lastFailedPayment', 'data'));
-});
-// ...existing code...
